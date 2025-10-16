045339edbf9e9fc625d52de4a3a7dc42190a26d9
Mayank Mandava <mayank@valdera.com>
2025-10-16 10:23:31 -0700

    update

--- keyboards/keebio/iris_lm/keymaps/xmonkee/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/xmonkee/keymap.c
@@ -13,11 +13,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+     QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    TL_UPPR,          TL_LOWR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -54,17 +54,17 @@ CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        
   )
 };
 
-const uint16_t PROGMEM lbrc_combo[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM lcbr_combo[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM lprn_combo[] = {KC_D, KC_F, COMBO_END};
-const uint16_t PROGMEM rbrc_combo[] = {KC_I, KC_O, COMBO_END};
-const uint16_t PROGMEM rcbr_combo[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM rprn_combo[] = {KC_J, KC_K, COMBO_END};
-combo_t key_combos[] = {
-    COMBO(lbrc_combo, KC_LBRC),
-    COMBO(lcbr_combo, KC_LCBR),
-    COMBO(lprn_combo, KC_LPRN),
-    COMBO(rbrc_combo, KC_RBRC),
-    COMBO(rcbr_combo, KC_RCBR),
-    COMBO(rprn_combo, KC_RPRN)
-};
+// const uint16_t PROGMEM lbrc_combo[] = {KC_W, KC_E, COMBO_END};
+// const uint16_t PROGMEM lcbr_combo[] = {KC_S, KC_D, COMBO_END};
+// const uint16_t PROGMEM lprn_combo[] = {KC_D, KC_F, COMBO_END};
+// const uint16_t PROGMEM rbrc_combo[] = {KC_I, KC_O, COMBO_END};
+// const uint16_t PROGMEM rcbr_combo[] = {KC_K, KC_L, COMBO_END};
+// const uint16_t PROGMEM rprn_combo[] = {KC_J, KC_K, COMBO_END};
+// combo_t key_combos[] = {
+//     COMBO(lbrc_combo, KC_LBRC),
+//     COMBO(lcbr_combo, KC_LCBR),
+//     COMBO(lprn_combo, KC_LPRN),
+//     COMBO(rbrc_combo, KC_RBRC),
+//     COMBO(rcbr_combo, KC_RCBR),
+//     COMBO(rprn_combo, KC_RPRN)
+// };
