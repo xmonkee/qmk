1c5a5f8a7f00a083cbeeac2b1b9438afae009dfe
Mayank Mandava <mayank@valdera.com>
2026-03-30 12:18:17 -0700

    Changes

--- keyboards/keebio/quefrency/keymaps/xmonkee/keymap.c
+++ keyboards/keebio/quefrency/keymaps/xmonkee/keymap.c
@@ -11,27 +11,37 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
+#define _FN2 2
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_65(
-    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
-    KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_LCTL, KC_LALT, KC_LGUI, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+  [_BASE] = LAYOUT_xmonkee(
+    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_HOME,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    			KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    KC_LALT, _______, MO(_FN2), MO(_FN1), KC_LGUI, KC_LSFT,			KC_SPC , MO(_FN1),  MO(_FN1),_______, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-  [_FN1] = LAYOUT_65(
-    QK_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______,
-    UG_TOGG, UG_NEXT, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
-  )
+  [_FN1] = LAYOUT_xmonkee(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______,
+	UG_TOGG,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, _______, _______, _______,
+    _______, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______, 			KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, _______, _______,
+    _______, _______, KC_LCBR, KC_RCBR, _______, _______, 			KC_UNDS, KC_EQL,  KC_MINS, KC_PLUS, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,  			KC_BSPC, MO(_FN2), _______, _______, _______, _______, _______
+  ),
+
+  [_FN2] = LAYOUT_xmonkee(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   	KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______,
+	UG_TOGG,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,          KC_CIRC, KC_7, KC_8, KC_9, KC_RPRN, _______, _______, _______, _______,
+    _______, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______, 			KC_LEFT, KC_4, KC_5, KC_6, KC_0, _______, _______, _______,
+    _______, _______, KC_LCBR, KC_RCBR, _______, _______, 			KC_UNDS, KC_1,  KC_2, KC_3, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______,          			_______, _______, _______, _______, _______, _______, _______, _______
+  ),
+
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
